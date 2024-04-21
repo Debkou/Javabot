ecb6939a42cf9dca8ccdc7135ac80967f4bd157f
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-04-21 20:24:06 +0200

    Update main_level_eins.ts

--- src/main_level_eins.ts
+++ src/main_level_eins.ts
@@ -14,7 +14,7 @@ const connection = mysql.createConnection({
 });
 
 // Verbindung zur Datenbank herstellen
-connection.connect((err) => {
+connection.connect((any err) => {
     if (err) {
         console.error('Fehler beim Verbinden zur Datenbank: ', err);
         return;
@@ -23,7 +23,7 @@ connection.connect((err) => {
 });
 
 // Beispielabfrage
-connection.query('SELECT * FROM users', (error, results, fields) => {
+connection.query('SELECT * FROM users', (any error, any results) => {
     if (error) {
         console.error('Fehler bei der Abfrage: ', error);
         return;
