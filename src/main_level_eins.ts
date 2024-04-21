8dafa91d4d8ba89989c08b0b31136cad1a129378
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-04-21 23:18:33 +0200

    Update main_level_eins.ts

--- src/main_level_eins.ts
+++ src/main_level_eins.ts
@@ -1,7 +1,7 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-import mysql from 'mysql';
+import * from 'mysql';
 
 // Verbindung zur Datenbank herstellen
 const connection = createConnection({
