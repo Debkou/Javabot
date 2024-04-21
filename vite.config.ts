c394ab6eb0ba6e1b5991a96ef7f25e71b41dc7c6
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-04-21 20:10:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 index: "./index.html",
-                  note: "src/dialog_note_test.html",
+                  note: "dialog_note_test.html",
                 ...getMapsScripts(maps),
             },
         },
