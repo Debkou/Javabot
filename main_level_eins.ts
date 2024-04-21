b398d67c016a6a43948d2b399c6e53b60e495daa
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-04-21 20:05:11 +0200

    Update main_level_eins.ts

--- main_level_eins.ts
+++ main_level_eins.ts
@@ -1,7 +1,7 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-
+const mysql = require('mysql');
 
 console.log('Script started successfully');
 
